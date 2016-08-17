cb8778fb472ff8b27c1941a8e5b12efdf86d6572
maxtang2013 <yllfever@gmail.com>
2016-08-17 12:20:22 +0800

    Integer Break (greedy)

---
+++ leetcode/343_IntegerBreak.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <queue>
+
+using namespace std;
+
+struct Entry {
+    int x, y;
+    int val;
+};
+
+struct Comparator {
+    bool operator()(const Entry& e1, const Entry& e2)
+    {
+        return e1.val > e2.val;
+    }
+};
+// priority_queue<Entry, vector<Entry>, Comparator> Q;
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        int ans = 1;
+        int cnt = 0;
+        
+        while (n > 4)
+        {
+            ans *= 3;
+            n -= 3;
+            ++cnt;
+        }
+        
+        switch (n) {
+            case 2:
+                if (cnt > 0) ans *= 2;
+                break;
+            case 3:
+                if (cnt == 0) ans = 2;
+                else ans *= 3;
+                break;
+            case 4:
+                ans *= 4;
+                break;
+        }
+        return ans;
+    }
+};
+
+template<typename T>
+void printVector(const vector<T>& list)
+{
+    int len = list.size();
+    for (int i = 0; i < len; ++i)
+    {
+        std::cout << list[i];
+        if (i < len - 1) std::cout << " ";
+        else std::cout << "\n";
+    }
+}
+
+int getLastBit(int n)
+{
+    //    Equavilent:
+    //    int mask = 0;
+    //    for (i = 0; i < 32; ++i)
+    //    {
+    //        mask = 1 << i;
+    //        if (bits & mask)
+    //        {
+    //            break;
+    //        }
+    //    }
+    //    return mask;
+    
+    return n & (-n);
+}
+
+void printBits(int n)
+{
+//    for (int i = 31; i >= 0; --i)
+//    {
+//        if (n & (1<<i)) std::cout << "1";
+//        else std::cout << "0";
+//    }
+//    std::cout << "\n";
+    
+    std::bitset<32> bits(n);
+    std::cout << bits << "\n";
+}
+
+int countBits(int n)
+{
+    int cnt = 0;
+    while (n)
+    {
+        ++cnt;
+        n = n & (n - 1);
+    }
+    return cnt;
+}
+
+void Test0()
+{
+    Solution sln;
+    
+    for (int i = 2; i < 59; ++i)
+    {
+        printf("%d\n", sln.integerBreak(i));
+    }
+}
+
+
+int main()
+{
+    Test0();
+    
+    return 0;
+}
