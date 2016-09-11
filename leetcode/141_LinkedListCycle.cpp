7c90cdc383b473f6510357f791bbcde0d0a2a42e
maxtang2013 <yllfever@gmail.com>
2016-09-11 11:15:42 +0800

    Linked list cycle, detect cycle start point

--- leetcode/141_LinkedListCycle.cpp
+++ leetcode/141_LinkedListCycle.cpp
@@ -24,6 +24,36 @@ public:
         } while (fast != slow && fast != NULL && slow != NULL);
         return fast != NULL && slow != NULL;
     }
+    
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL) return NULL;
+        ListNode *slow = head, *fast = head;
+        do {
+            slow = slow->next;
+            fast = fast->next;
+            if (fast != NULL) fast = fast->next;
+        } while (fast != slow && fast != NULL && slow != NULL);
+        
+        if (fast == NULL || slow == NULL) return NULL;
+        
+        while (slow != head) {
+            slow = slow->next;
+            head = head->next;
+        }
+        return head;
+        
+//        while (true) {
+//            if (fast == slow) slow = slow->next;
+//            
+//            while (slow != head && slow != fast) {
+//                slow = slow->next;
+//            }
+//            
+//            if (slow == head) break;
+//            head = head->next;
+//        }
+//        return head;
+    }
 };
 
 void Test0()
@@ -49,10 +79,29 @@ void Test1()
     cout << sln.hasCycle(nodes) << endl;
 }
 
+void Test2()
+{
+    Solution sln;
+    
+    ListNode nodes[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 4; ++i) {
+        nodes[i].next = nodes + i + 1;
+    }
+    nodes[4].next = nodes + 2;
+    ListNode* n = sln.detectCycle(nodes);
+    if (n != NULL) {
+        cout << n->val << endl;
+    } else
+        cout << "NULL" << endl;
+}
+
+
 int main()
 {
-    Test0();
-    Test1();
+//    Test0();
+//    Test1();
+    
+    Test2();
     
     return 0;
 }
