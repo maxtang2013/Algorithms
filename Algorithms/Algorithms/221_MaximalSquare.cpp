d5258d47a4062b46ac397a4947efae1f50e49cd1
maxtang2013 <yllfever@gmail.com>
2016-09-20 11:18:23 +0800

    MaximalSquare, DP, from O(m*n*min(m,n)) to O(m*n), memory usage can be reduced to O(n)

---
+++ Algorithms/Algorithms/221_MaximalSquare.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <queue>
+
+using namespace std;
+
+template<class T>
+T gcd(T m, T n) {
+    if (m < n) return gcd(n, m);
+    
+    if (n == 0) return m;
+    
+    return gcd(n, m%n);
+}
+
+struct Entry {
+    int x, y;
+    int val;
+};
+
+struct Comparator {
+    bool operator()(const Entry& e1, const Entry& e2)
+    {
+        return e1.val > e2.val;
+    }
+};
+// priority_queue<Entry, vector<Entry>, Comparator> Q;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct TreeNodeItem {
+    TreeNode* node;
+    int level;
+};
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+class Solution {
+public:
+};
+
+template<typename T>
+void printVector(const vector<T>& list)
+{
+    int len = list.size();
+    for (int i = 0; i < len; ++i)
+    {
+        std::cout << list[i];
+        if (i < len - 1) std::cout << " ";
+        else std::cout << "\n";
+    }
+    cout << endl;
+}
+template <typename T>
+void printBoard(const vector<vector<T>>& board) {
+    int n = (int)board.size();
+    for (int i = 0; i < n; ++i) {
+        int m = (int)board[i].size();
+        for (int j = 0; j < m; ++j) {
+            cout << board[i][j];
+            if (j < m - 1) cout << " ";
+            else cout << endl;
+        }
+    }
+    cout << endl;
+}
+
+void printfLinkedList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+
+int getLastBit(int n)
+{
+    //    Equavilent:
+    //    int mask = 0;
+    //    for (i = 0; i < 32; ++i)
+    //    {
+    //        mask = 1 << i;
+    //        if (bits & mask)
+    //        {
+    //            break;
+    //        }
+    //    }
+    //    return mask;
+    
+    return n & (-n);
+}
+
+void printBits(int n)
+{
+//    for (int i = 31; i >= 0; --i)
+//    {
+//        if (n & (1<<i)) std::cout << "1";
+//        else std::cout << "0";
+//    }
+//    std::cout << "\n";
+    
+    std::bitset<32> bits(n);
+    std::cout << bits << "\n";
+}
+
+int countBits(int n)
+{
+    int cnt = 0;
+    while (n)
+    {
+        ++cnt;
+        n = n & (n - 1);
+    }
+    return cnt;
+}
+
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+    if (root == NULL) return ans;
+    
+    vector<TreeNodeItem> S;
+    TreeNodeItem item = {root, 0};
+    S.push_back(item);
+    while (!S.empty()) {
+        item = S.back();
+        S.pop_back();
+        
+        if (item.level >= ans.size()) {
+            ans.push_back(vector<int>());
+        }
+        ans[item.level].push_back(item.node->val);
+        
+        if (item.node->right != NULL) {
+            TreeNodeItem item1 = {item.node->right, item.level + 1};
+            S.push_back(item1);
+        }
+        if (item.node->left != NULL) {
+            TreeNodeItem item1 = {item.node->left, item.level + 1};
+            S.push_back(item1);
+        }
+    }
+    return ans;
+}
+
+void TreeNodeTest()
+{
+    Solution sln;
+    
+    //        1
+    //    2      3
+    //  4   5  6   7
+    //8
+    
+    TreeNode nodes[] = {1, 2, 3, 4, 5, 6,7 ,8};
+    int len = sizeof(nodes) / sizeof(TreeNode);
+    for (int i = 0; i < len; ++i) {
+        if (i * 2 + 1 < len)
+            nodes[i].left = nodes + (i*2+1);
+        if (i*2+2<len)
+            nodes[i].right = nodes + (i*2+2);
+    }
+}
+
+void Test0()
+{
+    Solution sln;
+    
+}
+
+int main()
+{
+    Test0();
+    
+    return 0;
+}
