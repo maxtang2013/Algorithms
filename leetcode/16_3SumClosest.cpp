5e5b11149fe2cf9a2d10e90e03d865b66c2a36d2
maxtang2013 <yllfever@gmail.com>
2016-10-02 12:06:02 +0800

    3SumClosest, dfs, Pruning, Binary Search

--- leetcode/16_3SumClosest.cpp
+++ leetcode/16_3SumClosest.cpp
@@ -5,87 +5,91 @@
 
 using namespace std;
 
-struct Entry {
-    int x, y;
-    int val;
-};
 
-struct Comparator {
-    bool operator()(const Entry& e1, const Entry& e2)
-    {
-        return e1.val > e2.val;
-    }
-};
-// priority_queue<Entry, vector<Entry>, Comparator> Q;
+//Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+//
+//For example, given array S = {-1 2 1 -4}, and target = 1.
+//
+//The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
-class Solution {
+// Solution1, 29ms
+class Solution1 {
 public:
-
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        mBest = INT_MIN;
+        mArraySize = nums.size();
+        dfs(nums, 0, 3, target, 0);
+        return mBest;
+    }
+    
+private:
+    void dfs(const vector<int>& nums, int i, int k, int target, int curr) {
+        if (k == 0) {
+            if (mBest == INT_MIN || abs(target - curr) < mGap) {
+                mBest = curr;
+                mGap = abs(target - curr);
+            }
+            return;
+        }
+        
+        int start = i, end = mArraySize;
+        if (mBest != INT_MIN) {
+            
+            // Pruning 1, from 249ms to 162ms
+            if (k * nums[mArraySize-1] + curr <= target - mGap) return;
+            if (k * nums[i] + curr >= target + mGap) return;
+            
+            int lo = i, hi = mArraySize;
+            
+            // Pruning 2 Binary search for possible range, from 162ms to 42ms
+            
+            // Optimaze 4: avoid recalculate smallest in loop, from 29ms to 26ms.
+            int smallest = target - mGap - curr - (k-1) * nums[mArraySize - 1];
+            while (lo < hi) {
+                int mid = (lo + hi) / 2;
+                if (nums[mid] <= smallest) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid;
+                }
+            }
+            start = lo;
+            if (start >= mArraySize) return;
+            
+            int largest = target + mGap - curr - (k-1) * nums[start];
+            lo = i, hi = mArraySize;
+            while (lo < hi) {
+                int mid = (lo + hi) / 2;
+                if (nums[mid] >= largest) {
+                    hi = mid;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+            end = hi;
+        }
+        
+        for (int j = start; j < end; ++j) {
+            dfs(nums, j+1, k-1, target, curr+nums[j]);
+            
+            // Pruning 3. From 42ms to 29ms.
+            // skip same element.
+            while (j < mArraySize - 1 && nums[j+1] == nums[j]) ++j;
+        }
+    }
+    
+private:
+    int mArraySize;
+    int mBest;
+    int mGap;
 };
-
-template<typename T>
-void printVector(const vector<T>& list)
-{
-    int len = list.size();
-    for (int i = 0; i < len; ++i)
-    {
-        std::cout << list[i];
-        if (i < len - 1) std::cout << " ";
-        else std::cout << "\n";
-    }
-}
-
-int getLastBit(int n)
-{
-    //    Equavilent:
-    //    int mask = 0;
-    //    for (i = 0; i < 32; ++i)
-    //    {
-    //        mask = 1 << i;
-    //        if (bits & mask)
-    //        {
-    //            break;
-    //        }
-    //    }
-    //    return mask;
-    
-    return n & (-n);
-}
-
-void printBits(int n)
-{
-//    for (int i = 31; i >= 0; --i)
-//    {
-//        if (n & (1<<i)) std::cout << "1";
-//        else std::cout << "0";
-//    }
-//    std::cout << "\n";
-    
-    std::bitset<32> bits(n);
-    std::cout << bits << "\n";
-}
-
-int countBits(int n)
-{
-    int cnt = 0;
-    while (n)
-    {
-        ++cnt;
-        n = n & (n - 1);
-    }
-    return cnt;
-}
 
 void Test0()
 {
-    Solution sln;
+    Solution1 sln;
+    
 }
 
 int main()
